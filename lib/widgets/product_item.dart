e60e529f246e41d2332f70a51c4ce5c34086921e
Umar Hassan Khan <khanumarhassan26@gmail.com>
2022-06-28 19:31:34 +0500

    getting started with state management

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.favorite,
+            ),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+            icon: const Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.black54,
+        ),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(ProductDetailScreen.routeName, arguments: id);
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
